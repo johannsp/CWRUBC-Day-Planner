ba49fba79c35af7a08029a065696118941d04d46
John L Spetz <JohnLSpetz@zoho.com>
2020-06-26 18:18:44 -0400

    Checkpoint: HTML appearance largely stable

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
   
+  var plannerGrid = [];
+
+  function loadPlannerGrid() {
+    var stored = JSON.parse(localStorage.getItem("dayPlanner_PlannerGrid"));
+    if (stored) {
+      plannerGrid = stored;
+    } 
+    else {
+      for (var i = 9; i <= 17; i++) {
+        plannerGrid[i].timeHiClass = "";
+        plannerGrid[i].note = "";
+      }
+    }
+  }
+
+  function savePlannerGrid() {
+    if (plannerGrid) {
+      localStorage.setItem("dayPlanner_PlannerGrid",JSON.stringify(plannerGrid));
+    }
+  }
+
   function renderSchedule() {
-    var schdRow;
-    var schdTime;
     var currentDate = moment().format("dddd, MMMM Do");
+    var currentHour = moment().format("k");
     $("#currentDay").append(currentDate);
     for (var i = 9; i <= 17; i++) {
-      schdRow = $("<div>").addClass("row");
-      schdTime = $("<div>").addClass("col-md-1");
+      var row = ${"data-row-time"}
+      plannerGrid[i].timeHiClass = "";
+      plannerGrid[i].note = "";
     }
   }
 
